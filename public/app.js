fb3f3d65fc4450aeda6570757b1231ba7866515e
danilshabolta <danilshabolta@gmail.com>
2026-08-01 23:19:14 +0700

    Update default backend URL to new active Cloudflare tunnel

--- public/app.js
+++ public/app.js
@@ -222,7 +222,7 @@ function getChannel() {
   return 'cptponos';
 }
 
-const DEFAULT_BACKEND_HOST = 'attention-examples-neutral-visits.trycloudflare.com';
+const DEFAULT_BACKEND_HOST = 'conduct-crew-cache-independent.trycloudflare.com';
 
 // Определяем хост сервера расширения
 function getBackendHost() {
